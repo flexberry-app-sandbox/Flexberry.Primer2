bc6a73c2d2d13d47f48f0e4181b211c21bc0dc5e
Flexberry-man <mail@flexberry.net>
2023-12-19 04:57:45 +0000

    Flexberry code generation

---
+++ src/ember-app/app/locales/en/models/i-i-s-primer-forest.js
@@ -0,0 +1,37 @@
+export default {
+  projections: {
+    ForestE: {
+      name: {
+        __caption__: 'Name'
+      },
+      coordinates: {
+        __caption__: 'Coor'
+      }
+    },
+    ForestL: {
+      name: {
+        __caption__: 'Name'
+      },
+      coordinates: {
+        __caption__: 'Coordinates'
+      },
+      country: {
+        __caption__: 'Name',
+        name: {
+          __caption__: 'Name'
+        }
+      }
+    }
+  },
+  validations: {
+    coordinates: {
+      __caption__: 'Coor'
+    },
+    name: {
+      __caption__: 'Name'
+    },
+    country: {
+      __caption__: 'Name'
+    }
+  }
+};
